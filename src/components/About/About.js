078cfeeb3251f0397ed987b47bbf64f8fad782f2
diomandiakite92-ux <diomandiakite92@gmail.com>
2026-03-20 03:12:12 +0100

    changement logo

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,7 +5,6 @@ import Github from "./Github";
 import Techstack from "./Techstack";
 import Aboutcard from "./AboutCard";
 import laptopImg from "../../Assets/about.png";
-import Toolstack from "./Toolstack";
 
 function About() {
   return (
@@ -24,7 +23,7 @@ function About() {
               }}
             >
               <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
-                Know Who <strong className="purple">I'M</strong>
+                QUI <strong className="purple">SUIS-JE</strong> ?
               </h1>
               <Aboutcard />
             </Col>
@@ -41,11 +40,6 @@ function About() {
           </h1>
 
           <Techstack />
-
-          <h1 className="project-heading">
-            <strong className="purple">Tools</strong> I use
-          </h1>
-          <Toolstack />
 
           <Github />
         </Container>
--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+﻿import React from "react";
 import Card from "react-bootstrap/Card";
 import { ImPointRight } from "react-icons/im";
 
@@ -8,37 +8,76 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            Hi everyone! I’m <span className="purple">Soumyajit Behera</span>{" "}
-            from <span className="purple">Bhubaneswar, India</span>.
+            Bonjour à tous ! Je suis{" "}
+            <span className="purple">Dioman Diakite</span> originaire de{" "}
+            <span className="purple">Bamako, Mali</span>.
             <br />
-            I’m currently working as a{" "}
-            <span className="purple">Software Developer</span> at{" "}
-            <span className="purple">Juspay</span>.
-            <br />I hold an Integrated M.Sc. (IMSc) in{" "}
-            <span className="purple">Mathematics and Computing</span> from{" "}
-            <span className="purple">BIT Mesra</span>.
+            J'étudie actuellement le{" "}
+            <span className="purple">développement logiciel (L3)</span> à{" "}
+            <span className="purple">ESIIA</span>.
+            <br />
+            Je suis titulaire d'un{" "}
+            <span className="purple">
+              Master 1 en Informatique de Gestion
+            </span>{" "}
+            (ESGIC, Bamako) et d'un{" "}
+            <span className="purple">
+              Bachelor en Informatique Réseaux et Sécurité
+            </span>{" "}
+            (École Racine, Marrakech).
             <br />
             <br />
-            Outside of coding, I love engaging in activities that keep me
-            creative and inspired:
+            En dehors du code, j'ai développé une forte passion pour le{" "}
+            <span className="purple">trading</span>, et je suis convaincu que
+            les marchés financiers peuvent être un levier pour bâtir une
+            fortune.
+            <br />
+            <br />
+            Mon parcours a longtemps évité le développement. Après mon{" "}
+            <span className="purple">
+              bac en Sciences Exactes (2017, 13,78)
+            </span>
+            , je me suis orienté vers des filières où l'on codait peu :{" "}
+            <span className="purple">Informatique de Gestion</span>, puis{" "}
+            <span className="purple">Réseaux &amp; Sécurité</span>.
+            <br />
+            <br />
+            Le trading m'a amené à tester une stratégie manuelle avec un{" "}
+            <span className="purple">win rate d'environ 70%</span>. En voulant
+            automatiser, j'ai collecté beaucoup de données et j'ai découvert ma
+            passion pour la <span className="purple">data</span> et le
+            développement.
+            <br />
+            <br />
+            C'est dans cette optique que je me suis retrouvé en{" "}
+            <span className="purple">France</span> : je poursuis un{" "}
+            <span className="purple">bachelor en développement web</span> pour
+            consolider mes bases avant un{" "}
+            <span className="purple">master en Data</span>.
           </p>
 
           <ul>
             <li className="about-activity">
-              <ImPointRight /> Playing Games 🎮
+              <ImPointRight /> Parcours international :{" "}
+              <span className="purple">Mali -&gt; Maroc -&gt; France</span>
             </li>
             <li className="about-activity">
-              <ImPointRight /> Writing Tech Blogs ✍️
+              <ImPointRight /> Focus actuel :{" "}
+              <span className="purple">Développement + Data</span>
             </li>
             <li className="about-activity">
-              <ImPointRight /> Traveling and Exploring New Places 🌍
+              <ImPointRight /> Objectif :{" "}
+              <span className="purple">
+                Automatiser des stratégies de trading
+              </span>
             </li>
           </ul>
 
           <p style={{ color: "rgb(155 126 172)" }}>
-            "Strive to build things that make a difference!"{" "}
+            "Transformer la curiosité en compétences, et les données en
+            décisions."
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Dioman</footer>
         </blockquote>
       </Card.Body>
     </Card>
